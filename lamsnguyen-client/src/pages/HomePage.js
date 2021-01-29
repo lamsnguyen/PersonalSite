1f77dd951c48578a72dd16df694044ae119dd812
sonlam1609 <lamnguyendata@gmail.com>
2021-01-29 16:58:58 +0700

    update

--- lamsnguyen-client/src/pages/HomePage.js
+++ lamsnguyen-client/src/pages/HomePage.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from "react";
 //import { Image, Button } from "react-bootstrap";
 import { withRouter, useHistory, Link } from "react-router-dom";
+import ENG from "../assets/English.png";
+import VIE from "../assets/Vietnam.png";
+import "./HomePage.scss";
+
+
+function Language () {
+
+}
 
 function HomePage(prop) {
   return (
-    <div>
+    <div className="homepage-container">
       <p>
         Please choose a language
       </p>
-      <img className="English" src="../assets/English.png"></img>
+      <div className="language-bar">
+        <button>
+          <img src={ENG} alt="ENG"/>
+        </button>
+        <button>
+          <img src={VIE} alt="VIE"/>
+        </button>
+      </div>
     </div>
   )
 }
